6a8a39d291c45a2ee40352868da3eb479344647e
gamlerd13 <gamlerd13@gmail.com>
2024-06-19 23:58:58 -0600

    feat: add framework logos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -27,20 +27,28 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fall: {
+          "0%": { transform: "translateY(0%)" },
+          "100%": { transform: "translateY(50%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        slow: "fall 5s linear infinite",
+        medium: "fall 4s linear infinite",
+        fast: "fall 3s linear infinite",
       },
       backgroundImage: {
-        'hero-pattern': "url('https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg')",
+        "hero-pattern":
+          "url('https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg')",
       },
       height: {
-        '128': '32rem',
-      }
+        "128": "32rem",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
